d791cebddbef8d57fa765a557ee2149fd1ff650e
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-03-29 13:42:38 -0300

    Dados Novos

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-const soma = document.getElementById('somar')
+/*const soma = document.getElementById('somar')
 const sub = document.getElementById('sub')
 const multi = document.getElementById('multi')
-const divi = document.getElementById('divi') 
-let display = [0,1,2,3,4,5,6,7,8,9]
+const divi = document.getElementById('divi')
+const res = document.getElementById('res') 
+let display = [0,1,2,3,4,5,6,7,8,9]*/
+let dis = document.getElementById('display')
 
-function addnum1(){
-    if(addnum())
-
+function addnum(n){
+    let item = document.createElement('option')
+    item.tex = `${n}`
+    dis.appendChild(item)
+    res.innerHTML = ''
 }
+
+let ex = addnum(8)
+console.log(`${ex}`)
