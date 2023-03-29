dda9ead06be2454ee5b8ed90d897ec99bad56d74
Gustavo Jorge Geres <ezio.geres@gmail.com>
2023-03-29 17:07:57 -0300

    Dados Novos

--- script.js
+++ script.js
@@ -1,17 +1,99 @@
-/*const soma = document.getElementById('somar')
-const sub = document.getElementById('sub')
-const multi = document.getElementById('multi')
-const divi = document.getElementById('divi')
-const res = document.getElementById('res') 
-let display = [0,1,2,3,4,5,6,7,8,9]*/
-let dis = document.getElementById('display')
+const dis = document.getElementById('display')
+let alg = document.getElementsByClassName('num')
+var operator = ""
+let num = []
+let nums = []
+
 
 function addnum(n){
-    let item = document.createElement('option')
-    item.tex = `${n}`
-    dis.appendChild(item)
-    res.innerHTML = ''
+    if(nums.length > 0 && operator == ""){
+        reset()
+        nums = []
+    }
+    num.push(Number(n.value))
+    console.log(nums)
+    display(n.value)
 }
 
-let ex = addnum(8)
-console.log(`${ex}`)
+function display(input){
+    dis.value += input
+}
+
+function reset(){
+    dis.value = ''
+    num = []
+    console.log(num)
+    operator = ""
+}
+
+function somar(){
+    fechanum()
+    operator = "+"
+    dis.value += " + "
+}
+
+function sub(){
+    fechanum()
+    operator = "-"
+    dis.value += " - "
+}
+
+function multi(){
+    fechanum()
+    operator = "X"
+    dis.value +=" X "
+}
+
+function divi(){
+    fechanum()
+    operator = "÷"
+    dis.value += " ÷ "
+}
+
+
+function equal(){
+    fechanum()
+    let res = 0
+    switch (operator){
+        case "+":
+            res = nums[0] + nums[1]
+            nums = []
+            nums.push(res)
+        reset()  
+        display(res)
+        break;
+
+        case "X":
+            res = nums[0] * nums[1]
+            nums = []
+            nums.push(res)
+        reset()
+        display(res)
+        break;
+
+        case "-":
+            res = nums[0] - nums[1]
+            nums = []
+            nums.push(res)
+        reset()
+        display(res)
+        break;
+
+        case "÷":
+            res = nums[0]/nums[1]
+            nums = []
+            nums.push(res)
+        reset()
+        display(res)
+        break;
+    }
+}
+
+function fechanum(){
+    let item = ""
+    for(let i = 0; i < num.length; i++){
+        item += num[i]
+    }
+    nums.push(Number(item))
+    num = []
+}
